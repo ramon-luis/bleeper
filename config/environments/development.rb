63c6843b77e1ea6dbca009c30a504db4d797cecf
Ramon <ramonlrodriguez@gmail.com>
2018-03-01 17:36:52 -0600

    add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,10 +26,13 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
-  config.action_mailer.perform_caching = false
+  # Mailer
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  # host = 'ramon-luis-twitter-clone.herokuapp.com'
+  host = 'localhost:3000'
+  config.action_mailer.default_url_options = { host: host, protocol: 'https'}
+  config.action_mailer.perform_caching = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
